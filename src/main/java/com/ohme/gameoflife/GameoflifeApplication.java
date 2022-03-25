2d52839a3883887749e1bde1424182cbbb2ceb02
BrianHeaphy <brian.heaphy@concirrus.com>
2022-03-25 19:16:58 +0000

    Initial commit

---
+++ src/main/java/com/ohme/gameoflife/GameoflifeApplication.java
@@ -0,0 +1,13 @@
+package com.ohme.gameoflife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameoflifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameoflifeApplication.class, args);
+	}
+
+}
